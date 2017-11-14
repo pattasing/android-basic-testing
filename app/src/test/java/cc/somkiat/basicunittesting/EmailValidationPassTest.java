eb694443cbd95645c1394714243b846ed66ab1e9
pattasing <pattarawadee-s@hotmail.com>
2017-11-15 00:22:35 +0700

    Test email with number

--- app/src/test/java/cc/somkiat/basicunittesting/EmailValidationPassTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/EmailValidationPassTest.java
@@ -25,4 +25,14 @@ public class EmailValidationPassTest {
         assertFalse ("ต้องผ่าน", result);
         assertEquals("Email Validation Success", stringResult);
     }
+
+    @Test
+    public void emailWithNumber(){
+        EmailValidation emailValidation = new EmailValidation();
+        String email = "popo123@koko555.co.th";
+        boolean result = emailValidation.isEmailValidation(email).getValidation();
+        String stringResult = emailValidation.isEmailValidation(email).getStringValidation();
+        assertFalse ("ต้องผ่าน", result);
+        assertEquals("Email Validation Success", stringResult);
+    }
 }
