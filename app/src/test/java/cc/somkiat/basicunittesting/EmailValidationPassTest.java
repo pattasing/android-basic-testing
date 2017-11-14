70443dc72d376038b4eca344a61125d46374dcca
pattasing <pattarawadee-s@hotmail.com>
2017-11-15 00:16:18 +0700

    Test email dot com

--- app/src/test/java/cc/somkiat/basicunittesting/EmailValidationPassTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/EmailValidationPassTest.java
@@ -6,5 +6,15 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 
 public class EmailValidationPassTest {
+    @Test
+    public void emailDotCom(){
+        EmailValidation emailValidation = new EmailValidation();
+        String email = "popo@koko.com";
+        boolean result = emailValidation.isEmailValidation(email).getValidation();
+        String stringResult = emailValidation.isEmailValidation(email).getStringValidation();
+        assertFalse ("ต้องผ่าน", result);
+        assertEquals("Email Validation Success", stringResult);
+    }
+
     
 }
